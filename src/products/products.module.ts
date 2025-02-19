d9f967cbe549b2680d90fac822c80153c3b127d1
FarhadJs <farhadfallahi2004@gmail.com>
2025-02-19 03:37:58 +0330

    ✨ refactor: simplify multer configuration by using centralized multerOptions

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,29 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Product, ProductSchema } from './entities/product.entity';
 import { CategoriesModule } from '../categories/categories.module';
-import { v4 as uuidv4 } from 'uuid';
-import { extname } from 'path';
+
+import { multerOptions } from 'src/common/multer.config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
-    MulterModule.register({
-      storage: diskStorage({
-        destination: './uploads',
-        filename: (req, file, cb) => {
-          const uniqueSuffix = uuidv4() + extname(file.originalname);
-          cb(null, uniqueSuffix);
-        },
-      }),
-      limits: {
-        fileSize: 5 * 1024 * 1024, // 5MB
-      },
-    }),
+    MulterModule.register(multerOptions),
     CategoriesModule,
   ],
   controllers: [ProductsController],
